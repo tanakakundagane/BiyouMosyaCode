dc8f9721a9fec43a229974a73b54afc8567e80b2
Yosuke Iwata <s20970059@nucba.ac.jp>
2024-08-05 22:42:46 +0900

    ImageSliderの変更

--- src/app/components/ImageSlider.tsx
+++ src/app/components/ImageSlider.tsx
@@ -1,63 +1,44 @@
-// "use client";
-
-// import React, { useState, useEffect } from 'react';
-
-// const images = [
-//   '/images/sliderimage1.jpg',
-//   '/images/sliderimage2.jpg',
-//   '/images/sliderimage3.jpg',
-//   '/images/sliderimage4.jpg',
-//   '/images/sliderimage5.jpg',
-// ];
-
-// const ImageSlider = () => {
-//   const [currentIndex, setCurrentIndex] = useState(0);
-
-//   useEffect(() => {
-//     const interval = setInterval(() => {
-//       setCurrentIndex((prevIndex) => (prevIndex + 1) % images.length);
-//     }, 10000); // 10秒ごとに画像を切り替え
-
-//     return () => clearInterval(interval); // コンポーネントのクリーンアップ
-//   }, []);
-
-//   return (
-//     <div className="relative w-full h-screen overflow-hidden">
-//       {images.map((image, index) => (
-//         <div
-//           key={index}
-//           className={`absolute inset-0 w-full h-full transition-opacity duration-3000 ease-in-out ${index === currentIndex ? 'opacity-100' : 'opacity-0'}`}
-//           style={{ backgroundImage: `url(${image})`, backgroundSize: 'cover', backgroundPosition: 'center' }}
-//         ></div>
-//       ))}
-//     </div>
-//   );
-// };
-
-// export default ImageSlider;
-
 "use client";
 
 import React, { useState, useEffect } from 'react';
 
 const images = [
-  '/images/sliderimage1.jpg',
-  '/images/sliderimage2.jpg',
-  '/images/sliderimage3.jpg',
-  '/images/sliderimage4.jpg',
-  '/images/sliderimage5.jpg',
+  { src: '/images/sliderimage1.jpg',
+    description: '○○店\nLorem ipsum dolor sit amet consectetur adipisicing elit. Sequi, vero!\n Ipsum repellat soluta quod laborum natus rerum voluptates,\nnulla architecto eaque ea, molestiae quis amet ut iste quaerat minima sed?\n〒000-0000\n○○県〇〇市〇〇〇〇' },
+  { src: '/images/sliderimage2.jpg', 
+    description: '✖️✖️店\nLorem ipsum dolor sit amet consectetur adipisicing elit. Sequi, vero!\n Ipsum repellat soluta quod laborum natus rerum voluptates,\n〒000-0000\n✖️✖️県✖️市✖️✖️✖️✖️'},
+  { src: '/images/sliderimage3.jpg', 
+    description: '△店\nLorem ipsum dolor sit amet consectetur adipisicing elit. Sequi, vero!\n Ipsum repellat soluta quod laborum natus rerum voluptates,\nnulla architecto eaque ea, molestiae quis amet ut iste quaerat minima sed?\n〒000-0000\n△県△市△△△△' },
+  { src: '/images/sliderimage4.jpg', 
+    description: '◻︎店\nLorem ipsum dolor sit amet consectetur adipisicing elit. Sequi, vero!\n Ipsum repellat soluta quod laborum natus rerum voluptates,\n〒000-0000\n◻︎◻︎県◻︎◻︎市◻︎◻︎◻︎◻︎' },
+  { src: '/images/sliderimage5.jpg', 
+    description: '◻︎△店\nLorem ipsum dolor sit amet consectetur adipisicing elit. Sequi, vero!\n Ipsum repellat soluta quod laborum natus rerum voluptates,\nnulla architecto eaque ea, molestiae quis amet ut iste quaerat minima sed?\n〒000-0000\n◻︎△県◻︎△市◻︎◻︎◻︎◻︎' },
 ];
 
 const ImageSlider = () => {
   const [currentIndex, setCurrentIndex] = useState(0);
+  const [showText, setShowText] = useState(true);
 
   useEffect(() => {
     const interval = setInterval(() => {
-      setCurrentIndex((prevIndex) => (prevIndex + 1) % images.length);
+      setShowText(false);
+      setTimeout(() => {
+        setCurrentIndex((prevIndex) => (prevIndex + 1) % images.length);
+        setShowText(true);
+      }, 2000); // 3秒間のテキストのフェードアウト時間を待つ
     }, 10000); // 10秒ごとに画像を切り替え
 
     return () => clearInterval(interval); // コンポーネントのクリーンアップ
   }, []);
+
+  const formatDescription = (description: string) => {
+    return description.split('\n').map((line, index) => (
+      <React.Fragment key={index}>
+        {line}
+        <br />
+      </React.Fragment>
+    ));
+  };
 
   return (
     <div className="relative w-full h-screen overflow-hidden">
@@ -65,12 +46,14 @@ const ImageSlider = () => {
         <div
           key={index}
           className={`absolute inset-0 w-full h-full transition-opacity duration-3000 ease-in-out transform ${index === currentIndex ? 'opacity-100 scale-in' : 'opacity-0 scale-out'}`}
-          style={{ backgroundImage: `url(${image})`, backgroundSize: 'cover', backgroundPosition: 'center' }}
+          style={{ backgroundImage: `url(${image.src})`, backgroundSize: 'cover', backgroundPosition: 'center' }}
         ></div>
       ))}
+      <div className={`absolute bottom-5 left-20 m-4 p-2 text-white transition-opacity duration-3000 ease-in-out ${showText ? 'opacity-100' : 'opacity-0'}`}>
+        {formatDescription(images[currentIndex].description)}
+      </div>
     </div>
   );
 };
 
 export default ImageSlider;
-
